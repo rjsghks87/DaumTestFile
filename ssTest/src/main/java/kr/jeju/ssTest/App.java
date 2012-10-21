4a1a2b12f9618ee71d00ff1ae3d6d7364f71cafc
Gun <rjsghks2000@gmail.com>
2012-10-21 21:43:34 +0900

    Test File
    컴퓨터공학과 김건환, 테스트 파일을 첫 커밋해봅니다.

---
+++ ssTest/src/main/java/kr/jeju/ssTest/App.java
@@ -0,0 +1,13 @@
+package kr.jeju.ssTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
